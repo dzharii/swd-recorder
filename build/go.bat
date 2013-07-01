d39766a84cbbeeedb14fa1c2ba4194eef5486bab
dzhariy <dzhariy@gmail.com>
2013-07-02 00:40:03 +0300

    Fixed ILmerge option (.NET framewrork v4.5 issue)

--- build/go.bat
+++ build/go.bat
@@ -8,7 +8,7 @@ set SwdUiPath=%BinPath%\SwdPageRecorder.UI
 rd SwdPageRecorder_Latest /Q /S
 md SwdPageRecorder_Latest
 
-ilmerge\ILMerge.exe /targetplatform:v4 /t:winexe /out:SwdPageRecorder_Latest\SwdPageRecorder.exe %SwdUiPath%\SwdPageRecorder.UI.exe  %SwdUiPath%\HtmlAgilityPack.dll %SwdUiPath%\Newtonsoft.Json.dll %SwdUiPath%\SwdPageRecorder.WebDriver.dll %SwdUiPath%\WebDriver.dll %SwdUiPath%\WebDriver.Support.dll
+ilmerge\ILMerge.exe /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /t:winexe /out:SwdPageRecorder_Latest\SwdPageRecorder.exe %SwdUiPath%\SwdPageRecorder.UI.exe  %SwdUiPath%\HtmlAgilityPack.dll %SwdUiPath%\Newtonsoft.Json.dll %SwdUiPath%\SwdPageRecorder.WebDriver.dll %SwdUiPath%\WebDriver.dll %SwdUiPath%\WebDriver.Support.dll
 
 xcopy %SwdUiPath%\JavaScript  SwdPageRecorder_Latest\JavaScript /e/y/i
 copy ..\license.md SwdPageRecorder_Latest\license.txt /y
