1891fafa34908949658c0e8c3e9a77943c9a4b4b
dzhariy <dzhariy@gmail.com>
2013-07-07 18:23:47 +0300

    Build script improvements. Added new Dlls to ILMerge

--- build/go.bat
+++ build/go.bat
@@ -1,17 +1,30 @@
+@echo off
+@echo Building SwdPageRecorder...
+
 set SlnPath=..\SwdPageRecorder\SwdPageRecorder.sln
 set BinPath=..\Bin
 set SwdUiPath=%BinPath%\SwdPageRecorder.UI
 
-@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild %SlnPath% /t:clean
-@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild %SlnPath%
+set STDOUT_DEFAULT=nul
+echo Hello! >%STDOUT_DEFAULT%
 
-rd SwdPageRecorder_Latest /Q /S
-md SwdPageRecorder_Latest
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild %SlnPath% /t:clean >>%STDOUT_DEFAULT%
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild %SlnPath% >>%STDOUT_DEFAULT%
 
-ilmerge\ILMerge.exe /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /t:winexe /out:SwdPageRecorder_Latest\SwdPageRecorder.exe %SwdUiPath%\SwdPageRecorder.UI.exe  %SwdUiPath%\HtmlAgilityPack.dll %SwdUiPath%\Newtonsoft.Json.dll %SwdUiPath%\SwdPageRecorder.WebDriver.dll %SwdUiPath%\WebDriver.dll %SwdUiPath%\WebDriver.Support.dll
+rd SwdPageRecorder_Latest /Q /S  >>%STDOUT_DEFAULT%
+md SwdPageRecorder_Latest >>%STDOUT_DEFAULT%
 
-xcopy %SwdUiPath%\JavaScript  SwdPageRecorder_Latest\JavaScript /e/y/i
-copy ..\license.md SwdPageRecorder_Latest\license.txt /y
+set MERGE_LIBS1=%SwdUiPath%\HtmlAgilityPack.dll %SwdUiPath%\Newtonsoft.Json.dll %SwdUiPath%\SwdPageRecorder.WebDriver.dll %SwdUiPath%\WebDriver.dll %SwdUiPath%\WebDriver.Support.dll
+set MERGE_LIBS2=%SwdUiPath%\RazorEngine.dll %SwdUiPath%\System.Web.Razor.dll
+
+ilmerge\ILMerge.exe /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /t:winexe /out:SwdPageRecorder_Latest\SwdPageRecorder.exe %SwdUiPath%\SwdPageRecorder.UI.exe  %MERGE_LIBS1% %MERGE_LIBS2% >>%STDOUT_DEFAULT%
+
+xcopy %SwdUiPath%\JavaScript  SwdPageRecorder_Latest\JavaScript /e/y/i >>%STDOUT_DEFAULT%
+xcopy %SwdUiPath%\CodeTemplates  SwdPageRecorder_Latest\CodeTemplates /e/y/i >>%STDOUT_DEFAULT%
+
+copy ..\license.md SwdPageRecorder_Latest\license.txt /y >>%STDOUT_DEFAULT%
+
+@echo Build completed.
 
 
 
