e6d4c829708dd7792904a056e97396cc448a0982
Dmytro Zharii <dzhariy@gmail.com>
2015-09-04 00:34:42 -0700

    add(readme) arrows

--- SwdPageRecorder/SwdPageRecorder.UI/SwdMain/CSharpPageObjectGenerator.cs
+++ SwdPageRecorder/SwdPageRecorder.UI/SwdMain/CSharpPageObjectGenerator.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
+﻿using System.IO;
 using RazorEngine;
-using SwdPageRecorder.UI.CodeGeneration;
 using SwdPageRecorder.WebDriver;
 
 namespace SwdPageRecorder.UI
