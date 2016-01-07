0dc04f4c25d1be3c798dbea114cf11386829a1f4
Dmytro Zharii <dzhariy@gmail.com>
2016-01-07 01:03:24 -0800

    add missed jar in selenium server batch file

--- SwdPageRecorder/SwdPageRecorder.WebDriver/start_selenium_server.bat
+++ SwdPageRecorder/SwdPageRecorder.WebDriver/start_selenium_server.bat
@@ -2,6 +2,6 @@
 @set CHROME=-Dwebdriver.chrome.driver="%cd%\chromedriver.exe"
 @set PHANTOM=-Dphantomjs.binary.path="%cd%\phantomjs.exe"
 
-@set SELENIUM_SERVER_EXE=selenium-server-standalone-2.48.2
+@set SELENIUM_SERVER_EXE=selenium-server-standalone-2.48.2.jar
 
 start "%SELENIUM_SERVER_EXE%" java %PHANTOM% %CHROME% %IE% -jar %SELENIUM_SERVER_EXE%
